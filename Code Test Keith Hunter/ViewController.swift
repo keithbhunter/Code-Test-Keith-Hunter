334ad4b15d8d653736fa44ffe227b2b1b8deb2b3
Keith Hunter <keith.hunter@outlook.com>
2018-12-21 09:57:29 -0500

    Created Xcode project.

---
+++ Code Test Keith Hunter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Test Keith Hunter
+//
+//  Created by Keith Hunter on 12/21/18.
+//  Copyright © 2018 Keith Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
